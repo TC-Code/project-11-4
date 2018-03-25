d1f298b423e5f712cf1db5a0ea69e4f8afd920b1
Tomasz Chowaniec <35344311+TC-Code@users.noreply.github.com>
2018-03-25 10:24:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ Phone.prototype.printInfo = function() {
 };
 var iPhone6S = new Phone("Apple", "iPhone 6S", 2250, "silver");
 iPhone6S.printInfo();
-var SamsungGalaxyS6 = new Phone("Samsung", "Galaxy S6", 899, "gold");
-SamsungGalaxyS6.printInfo();
-var OnePlusOne = new Phone("OnePlus", "One", 1650, "silver");
-OnePlusOne.printInfo();
+var samsungGalaxyS6 = new Phone("Samsung", "Galaxy S6", 899, "gold");
+samsungGalaxyS6.printInfo();
+var onePlusOne = new Phone("OnePlus", "One", 1650, "silver");
+onePlusOne.printInfo();
